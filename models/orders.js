7fc1cbe835382169e255945785ba0efcaae31ddb
unknown <srivastava11735@gmail.com>
2023-02-04 16:50:22 +0530

    category

---
+++ models/orders.js
@@ -0,0 +1,33 @@
+// import 'mongoose';
+
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const Category = require("./category");
+
+const orderSchema = new Schema({
+
+    OrderId:String,
+    TotalAmount:Number,
+    OrderStatus:String,
+    Date:Date(),
+    OrderDetails:[{
+        Product:{
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Product',
+        },
+        Quantity:Number
+    }
+    ],
+    Products:[{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Product',
+    }],
+    User:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+    },
+});
+
+const Order = mongoose.model("Order", orderSchema);
+
+module.exports = Order;
