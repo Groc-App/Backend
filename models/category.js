2120e5662b90ab912010d2a873512af9f5fac30d
Prakhar <srivastava11735@gmail.com>
2023-02-05 17:11:20 +0530

    json

--- models/category.js
+++ models/category.js
@@ -11,7 +11,7 @@ const categorySchema = new Schema({
         type: mongoose.Schema.Types.ObjectId,
         ref: Product,
     },
-    Q
+
 });
 
 const Category = mongoose.model("Category", categorySchema);
