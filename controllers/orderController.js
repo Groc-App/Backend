480873f5f458fd1e0c6a90368b23e3130bd8b32c
Arjun <arjundagar28@gmail.com>
2023-02-28 21:44:10 +0530

    order place done

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -5,48 +5,56 @@ const { findByIdAndDelete, findByIdAndUpdate } = require("../models/category");
 const Category = require("../models/category");
 const Product = require("../models/product");
 const Order = require("../models/orders");
+const Address = require("../models/address");
 
 exports.fetchallOrdersbyUserId = async (req, res) => {
   try {
     const { userid } = req.params;
 
-    console.log(userid)
+    console.log(userid);
     const data = await User.findOne({ Number: userid }).populate({
       path: "Order",
-      populate: {
-        path: "OrderDetails.Product",
-        model: "Product",
-      },
-    })
+      populate: [
+        {
+          path: "OrderDetails.Product",
+          model: "Product",
+        },
+        {
+          path: "Addres",
+          model: "Address",
+        },
+      ],
+    });
 
     if (!data) {
       return res.status(200).json({
-        message: "No Data Found"
-      })
+        message: "No Data Found",
+      });
     }
 
     res.status(200).json({
       message: "Success",
-      data
-    })
-
+      data,
+    });
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
 };
 
-
-
 // ordere id generate krvani h
 
 exports.createOrder = async (req, res) => {
   try {
-    const { tamount, userid, orderdetail, address } = req.body;
+    const { tamount, userid, orderdetail, addressid } = req.body; // address map string bhej rha hu to ek baar check kr liyo krunyi ab ref use kr liya
 
     console.log(req.body);
-    //order detail = [{productid, quantity}]
 
     const usar = await User.findOne({ Number: userid });
+    const adress = await Address.findById(addressid);
+
+    if (!adress) {
+      return res.status(400).json({ error: "num address found" });
+    }
 
     const neworder = new Order({
       TotalAmount: tamount,
@@ -54,7 +62,7 @@ exports.createOrder = async (req, res) => {
       Date: Date.now(),
       OrderDetails: orderdetail,
       User: usar._id,
-      Address: address
+      Addres: adress._id,
     });
 
     console.log("New Order", neworder);
@@ -72,13 +80,55 @@ exports.createOrder = async (req, res) => {
 
     res.status(200).json({
       message: "Order Create Successfully",
-
-    })
-
+    });
   } catch (error) {
-
     res.status(400).json({ error: error.message });
-
   }
 };
 
+exports.fetchallAllOrdersbyStatus = async (req, res) => {
+  try {
+    const { status } = req.params;
+
+    const data = await Order.find({ OrderStatus: status }).populate("OrderDetails.Product").populate("Addres");
+
+    if (!data) {
+      return res.status(200).json({
+        message: "No Data Found",
+        data: data,
+      });
+    }
+
+    return res.status(200).json({
+      message: "Success",
+      data: data,
+    });
+  } catch (error) {
+    return res.status(400).json({ error: error.message });
+  }
+};
+
+exports.updateorderstatus = async (req, res) => {
+  try {
+    const { orderid, status } = req.body;
+    console.log(status);
+
+    const order = await Order.findById(orderid);
+
+    if (!order) {
+      return res.status(300).json({
+        message: "No order found in database",
+      });
+    }
+
+    order.OrderStatus = status;
+    await order.save();
+
+    return res.status(200).json({
+      message: "Successfully updated status",
+      data: order,
+    });
+  } catch (error) {
+    return res.status(400).json({ error: error.message });
+  }
+};
