df84796eac235afc33026fd92c36ff01cad743ec
unknown <srivastava11735@gmail.com>
2023-03-20 21:36:49 +0530

    Offer

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -94,7 +94,7 @@ exports.addUser = async (req, res, next) => {
     /* -------------------------------------------------------------------------- */
 
     if (!user) {
-      return res.status(404).send({
+      return res.status(404).json({
         message: "No User Created",
         data: null,
       });
